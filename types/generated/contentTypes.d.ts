c370579cc03ee5782f43036dc689746bc7ad8a42
“CharlesCahill123” <charles.cahill.intern@sofwerx.org>
2024-04-23 19:35:05 -0400

    trying to fix auto id

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -804,7 +804,6 @@ export interface ApiBookBook extends Schema.CollectionType {
     isbn: Attribute.String & Attribute.Required;
     author: Attribute.String & Attribute.Required;
     description: Attribute.Text;
-    book_id: Attribute.UID<'api::book.book', 'name'>;
     cover: Attribute.Media;
     pdf: Attribute.Media & Attribute.Required;
     public: Attribute.Boolean & Attribute.Required;
@@ -812,6 +811,7 @@ export interface ApiBookBook extends Schema.CollectionType {
     category: Attribute.String;
     publisher: Attribute.String;
     user_id: Attribute.String;
+    book_id: Attribute.UID<'api::book.book', 'name'> & Attribute.Required;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
@@ -868,17 +868,14 @@ export interface ApiLibraryUserLibraryUser extends Schema.CollectionType {
     username: Attribute.String & Attribute.Required;
     email: Attribute.Email;
     profilepic: Attribute.Media;
-    user_id: Attribute.UID &
-      Attribute.Required &
-      Attribute.SetMinMaxLength<{
-        minLength: 10;
-      }>;
     books: Attribute.Relation<
       'api::library-user.library-user',
       'oneToMany',
       'api::book.book'
     >;
     password: Attribute.String & Attribute.Required;
+    user_id: Attribute.UID &
+      Attribute.CustomField<'plugin::strapi-advanced-uuid.uuid'>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
